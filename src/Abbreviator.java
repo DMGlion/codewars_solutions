2c6b73ab12697a97a2e9304df347550dbed5ff00
David Moya <davidmgarcia6@gmail.com>
2019-09-23 23:58:20 +0100

    Word a10n (abbreviation) java

---
+++ src/Abbreviator.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Abbreviator {
+    public static String abbreviate(String string) {
+        StringBuilder abbStr = new StringBuilder();
+        int count=-1;
+        for(int i=0;i<string.length();i++){
+            if(!Character.isLetter(string.charAt(i))){
+                abbStr.append(string.charAt(i));
+                count=-1;
+            }else if(count==-1){
+                abbStr.append(string.charAt(i));
+                count++;
+            }else if(Character.isLetter(string.charAt(i))&&i+1<string.length()&&Character.isLetter(string.charAt(i+1))) count++;
+            else if(count>1) abbStr.append(count+""+string.charAt(i));
+            else abbStr.append(count==1?string.substring(i-1,i+1):string.charAt(i));
+        }
+        return abbStr.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(abbreviate("monolithic; doggy-cat the sat"));
+    }
+    // monolithic; doggy-cat the sat
+    // m8c; d3y-cat the sat
+}
